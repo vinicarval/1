e85a2540c4d102ae848c991b98427fee6845b8c8
Flavio Rossi <flaviowenzel1996@gmail.com>
2016-12-04 02:31:49 -0200

    Java - 1037/

---
+++ 1037/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner s = new Scanner(System.in);
+        
+        double valor = s.nextDouble();
+        verificar(valor);
+    }
+    public static void verificar(double valor)
+    {
+        if(valor >= 0 & valor <= 25)
+            System.out.println("Intervalo [0,25]");
+        else if(valor > 25 & valor <= 50)
+            System.out.println("Intervalo (25,50]");
+        else if(valor > 50 & valor <= 75)
+            System.out.println("Intervalo (50,75]");
+        else if(valor > 75 & valor <= 100)
+            System.out.println("Intervalo (75,100]");
+        else
+            System.out.println("Fora de intervalo");
+    }
+}
